2b75c37d03d72cb366cdfd721ace71a60fd4383a
surfacebook <jakobreichl@gmail.com>
2019-07-10 15:20:00 +0200

    änderung

--- Numpy/numpy_examples.py
+++ Numpy/numpy_examples.py
@@ -3,3 +3,16 @@ import numpy as np
 a = np.array([1, 2, 3, 4, 5])
 print(a)
 print('mean:' + str(np.mean(a)))
+
+b = np.array([[2,3,4],[12,13,14]])
+print('b.size={0:3d}'.format(b.size))
+print('np.size(b,0)={0:3d}'.format(np.size(b,0)))
+print('np.size(b,1)={0:3d}'.format(np.size(b,1)))
+
+print(b.shape)
+print('b.shape[0]={0:3d}'.format(b.shape[0]))
+print('b.shape[1]={0:3d}'.format(b.shape[1]))
+print('b.shape[1]={0:3d}'.format(b.shape[1]))
+print('b.shape[1]={0:3d}'.format(b.shape[1]))
+print('b.shape[1]={0:3d}'.format(b.shape[1]))
+print('b.shape[1]={0:3d}'.format(b.shape[1]))
